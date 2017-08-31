a261f93ad6b88b127a8991b167dd38bd98927e77
Seosamh Cahill <seo.cahill@gmail.com>
2017-08-31 12:18:29 +0200

    Add response and error logs

--- dummy_app.rb
+++ dummy_app.rb
@@ -1,18 +1,25 @@
 require 'net/http'
 require 'json'
+require 'logger'
 
 module DummyApp
   class Base
+
+    def init
+      @log = Logger.new(File.new(File.join(__dir__, "logs", "error.log")),'w')
+    end
 
     def request_validation(filename)
       uri = URI("http://arelle:8080/rest/xbrl/validation?file=/ixbrl/#{filename}&media=json")
       begin
         request = Net::HTTP.get(uri)
         response = JSON.parse(request)
-        # puts JSON.pretty_generate(response)
+        File.open(File.join(__dir__, "logs", filename.gsub("html", "json")), 'w') do |f|
+          f.write(JSON.pretty_generate(response))
+        end
         response
       rescue Exception => e
-        e.to_s
+        @log.error(e)
       end
     end
   end
